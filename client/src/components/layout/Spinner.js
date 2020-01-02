8ebed9b1dd34ca941cff2f606e8e563f344c75e4
Bryce McMath <bryce.j.mcmath@gmail.com>
2020-01-02 10:22:54 -0800

    Added actions and reducers for posts and profiles

---
+++ client/src/components/layout/Spinner.js
@@ -0,0 +1,8 @@
+import React, { Fragment } from "react";
+import spinner from "../../../assets/spinner.gif";
+
+export default () => (
+  <Fragment>
+    <img className="spinner" src={spinner} alt="Loading..." />
+  </Fragment>
+);
