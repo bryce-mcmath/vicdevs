19c9372948dddfee9136f1fcd04e32c4d6bd19ae
Bryce McMath <bryce.j.mcmath@gmail.com>
2020-03-11 21:58:13 -0700

    Update buildspec

--- server/index.js
+++ server/index.js
@@ -4,6 +4,15 @@ const connectDB = require('./config/db');
 const helmet = require('helmet');
 const xss = require('xss-clean');
 const app = express();
+const fs = require('fs');
+
+fs.readdir(__dirname, function(err, items) {
+	console.log('items: ', items);
+
+	for (const i = 0; i < items.length; i++) {
+		console.log('item:', items[i]);
+	}
+});
 
 // Connect to database
 connectDB();
